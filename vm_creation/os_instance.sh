3bb68c3ea10d7a269ceed02694db5378a4b29d0b
Alberto Damo <alberto.damo@gmail.com>
2024-02-27 22:57:38 +0100

    finished OS configuration

--- vm_creation/os_instance.sh
+++ vm_creation/os_instance.sh
@@ -1,12 +1,13 @@
 #partition creation
 echo "label: dos" | sfdisk /dev/vda
 echo ";" | sfdisk /dev/vda
+sfdisk -A /dev/vda 1
 
 mount /dev/vda1 /mnt
 
 pacman -Sy
 
-pacstrap -K /mnt base linux linux-firmware dhcp dhcpcd iw grub vim bash
+pacstrap -K /mnt base linux linux-firmware dhcp dhcpcd iw grub vim bash go
 
 mkdir /mnt/boot
 
@@ -21,3 +22,8 @@ chroot /mnt/ systemctl enable dhcpcd
 
 chroot /mnt grub-install /dev/vda
 chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+chroot /mnt passwd -d root
+chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+
+chroot /mnt sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /etc/default/grub
