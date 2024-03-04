43c0227f2f72acc251b7e64c8d0af15df455f7b6
Alberto Damo <alberto.damo@gmail.com>
2024-03-04 23:15:25 +0100

    virtual environment setup and project organization

---
+++ env_setup/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+first_user=$(whoami)
+wiki_libvirt="https://wiki.archlinux.org/title/libvirt"
+wiki_virt_manager="https://wiki.archlinux.org/title/Virt-manager"
+
+if [[ $first_user == "root" ]]; then
+    echo "you must not be root to execute this program"
+    exit
+fi
+
+
+echo "installing the correct packages"
+sudo pacman -Sy libvirt virt-manager iptables-nft dnsmasq virt-viewer dmidecode openbsd-netcat
+
+echo "enabling the deamon for libvirtd"
+sudo systemctl enable --now libvirtd
+
+echo "adding libvirt group to user $first_user"
+sudo usermod  -aG libvirt $first_user
+
+echo "to open the gui to manage the virtual machine use virt-manager"
+echo "for more infos check:"
+echo $wiki_libvirt
+echo $wiki_virt_manager
+
+
+answer="p"
+while [ $answer != "y" ] && [ $answer != "n" ]; do
+    echo -n "do you want to open the two links to the wiki?:[y/n]"
+    read answer
+done
+
+if [[ $answer == "y" ]]; then
+    xdg-open $wiki_virt_manager
+    xdg-open $wiki_libvirt
+fi
+
+exit
