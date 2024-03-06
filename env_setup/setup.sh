27159e51d99d8a2237ed34e7e8900e73913027b2
Alberto Damo <alberto.damo@gmail.com>
2024-03-06 23:40:13 +0100

    tested setup script, added link in file, added non confirmation for package

--- env_setup/setup.sh
+++ env_setup/setup.sh
@@ -3,6 +3,7 @@
 first_user=$(whoami)
 wiki_libvirt="https://wiki.archlinux.org/title/libvirt"
 wiki_virt_manager="https://wiki.archlinux.org/title/Virt-manager"
+link_iso=$(cat ./link_download_live)
 
 if [[ $first_user == "root" ]]; then
     echo "you must not be root to execute this program"
@@ -11,7 +12,7 @@ fi
 
 
 echo "installing the correct packages"
-sudo pacman -Sy libvirt virt-manager iptables-nft dnsmasq virt-viewer dmidecode openbsd-netcat
+sudo pacman -Sy libvirt virt-manager iptables-nft dnsmasq virt-viewer dmidecode openbsd-netcat --noconfirm
 
 echo "enabling the deamon for libvirtd"
 sudo systemctl enable --now libvirtd
@@ -20,11 +21,11 @@ echo "adding libvirt group to user $first_user"
 sudo usermod  -aG libvirt $first_user
 
 echo "getting and coping the install iso in the dir /var/lib/libvirt/images/"
-wget https://download1526.mediafire.com/3f24vvgj9zwg4Gc18H9p4VxkEJjA4WeXsID9RlXQoPTP3B7weJfEdxpwyl9u-XKfhirBPkPfrOdNqSsW5HyrtxbHlruvivcd70EkEvHdOA35cpHviRP772jjuBCsPfg7v1ewiXHEERIo8yMvn5zdeqBcQ12sdnEX-i-AKS2MNoP_Zcw/m7oyfnz9q50f0ct/raft_live_install.iso
+wget $link_iso
 sudo cp ./raft_live_install.iso /var/lib/libvirt/images/
 
 echo "installing ssh"
-sudo pacman -Sy openssh
+sudo pacman -Sy openssh --noconfirm
 
 echo "coping ssh key for remote node access"
 cp ./raft_node_key ~/.ssh
