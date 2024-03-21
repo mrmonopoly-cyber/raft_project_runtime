3584045bb02acc9959e0a78a6614f9604890089a
Alberto Damo <alberto.damo@gmail.com>
2024-03-21 19:41:53 +0100

    debugging

--- raft/internal/node/address/address.go
+++ raft/internal/node/address/address.go
@@ -9,52 +9,49 @@ import (
 
 const sectorsNumber = 4
 
-type NodeAddress interface
-{
-    GetIp() string
-    GetPort() string
+type NodeAddress interface {
+	GetIp() string
+	GetPort() string
 }
 
-type nodeAddress struct
-{
-    sectors [sectorsNumber] uint8
-    port    uint16
+type nodeAddress struct {
+	sectors [sectorsNumber]uint8
+	port    uint16
 }
 
 func (this nodeAddress) GetIp() string {
-    var ipAddr string = ""
-    var num uint8 = this.sectors[0]
-    
-    for i:= 0;  i< sectorsNumber-1; i++ {
-        num = this.sectors[i]
-        ipAddr += strconv.Itoa(int(num))
-        ipAddr += "."
-    }
-    ipAddr += strconv.Itoa(int(num))
-    return ipAddr
+	var ipAddr string = ""
+	var num uint8 = this.sectors[0]
+
+	for i := 0; i < sectorsNumber-1; i++ {
+		num = this.sectors[i]
+		ipAddr += strconv.Itoa(int(num))
+		ipAddr += "."
+	}
+	ipAddr += strconv.Itoa(int(num))
+	return ipAddr
 }
 
 func (this nodeAddress) GetPort() string {
-    log.Println("returning port: ", this.port)
-    // return strconv.FormatUint(uint64(this.port),10)
-    return "8080"
+	log.Println("returning port: ", this.port)
+	return strconv.Itoa(int(this.port))
 }
 
-func NewNodeAddress(ipAddr string, port string) NodeAddress{
-    var sectorsStr = strings.Split(ipAddr, ".")
-    var node nodeAddress
+func NewNodeAddress(ipAddr string, port string) NodeAddress {
+	var sectorsStr = strings.Split(ipAddr, ".")
+	var node nodeAddress
 
-    for i := 0; i < sectorsNumber; i++ {
-        out, err := strconv.Atoi(sectorsStr[i])
-        if err != nil {
-            fmt.Println("Error:", err)
-            return nil
-        }
-        node.sectors[i] = uint8(out)
-    }
+	for i := 0; i < sectorsNumber; i++ {
+		out, err := strconv.Atoi(sectorsStr[i])
+		if err != nil {
+			fmt.Println("Error:", err)
+			return nil
+		}
+		node.sectors[i] = uint8(out)
+	}
 
-    var port_int, _ = strconv.Atoi(port)
-    node.port = uint16(port_int)
+	var port_int, _ = strconv.Atoi(port)
+	node.port = uint16(port_int)
 
-    return &node
+	return &node
 }
