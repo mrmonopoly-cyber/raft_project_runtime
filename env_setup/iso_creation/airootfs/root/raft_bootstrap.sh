180b9d7081a65a218a57643ecfdd335291df49af
Alberto Damo <alberto.damo@gmail.com>
2024-03-21 17:02:01 +0100

    bugfix bootstrap

--- env_setup/iso_creation/airootfs/root/raft_bootstrap.sh
+++ env_setup/iso_creation/airootfs/root/raft_bootstrap.sh
@@ -3,22 +3,23 @@
 work_dir=/root
 mount_p=$work_dir/mount
 raft_dir=$mount_p/raft
-my_ip_pos=$raft_dir/my_ip
 raft_executable=raft
 raft_branch=raft_executables 
 raft_url=https://github.com/mrmonopoly-cyber/raft_project_runtime.git 
+my_ip_pos=$raft_dir/my_ip
+others_ip_pos=$raft_dir/others_ip
 my_ip=""
 
 while [[ -z $my_ip ]]; do
     my_ip=$(ip addr show | grep 192.168 | cut -d' ' -f 6 | cut -d'/' -f1);
 done
 
-if [[ ! -b $raft_dir ]];
+if [[ -e $raft_dir ]];
 then
     rm -rf $raft_dir
 fi
 
-if [[ ! -b $raft_dir ]]; then
+if [[ ! -e $raft_dir ]]; then
     mkdir $raft_dir
 fi
 
@@ -36,6 +37,10 @@ echo "repo cloned"
 touch $my_ip_pos
 echo "$my_ip" > $my_ip_pos
 echo "setted ips"
+
+while [[ ! -e $others_ip_pos ]]; do
+    sleep 1
+done
 
 ##start main program of raft
 
