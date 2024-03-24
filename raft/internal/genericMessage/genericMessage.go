9aa04e5b954dc96fce859c55a761d2a1046c384c
Alberto Damo <alberto.damo@gmail.com>
2024-03-24 16:46:46 +0100

    test with payload

--- raft/internal/genericMessage/genericMessage.go
+++ raft/internal/genericMessage/genericMessage.go
@@ -35,16 +35,16 @@ func Decode(raw_mex []byte) (*rpcs.Rpc){
 
 func Encode(mex *rpcs.Rpc) ([]byte,error){
     var err error
-    // var rawByte []byte
+    var rawByte []byte
     var rawByteToSend []byte
     var genericMessage protobuf.Entry
 
-    // rawByte, err= (*mex).Encode()
-    // genericMessage.Payload = rawByte
+    rawByte, err= (*mex).Encode()
+    genericMessage.Payload = rawByte
 
-    // if err != nil {
-    //     log.Panicln("error encoding this message :", (*mex).ToString())
-    // }
+    if err != nil {
+        log.Panicln("error encoding this message :", (*mex).ToString())
+    }
 
     switch (*mex).(type){
     case *AppendEntryRpc.AppendEntryRpc:
