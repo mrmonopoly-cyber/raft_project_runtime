6c74bae074745a17f43ee2a5896b18cad1d919b0
Alberto Damo <alberto.damo@gmail.com>
2024-03-29 14:49:41 +0100

    updated run code

--- raft/raft/internal/rpcs/AppendEntryRpc/AppendEntryRpc.go
+++ raft/raft/internal/rpcs/AppendEntryRpc/AppendEntryRpc.go
@@ -56,6 +56,9 @@ func NewAppendEntryRPC(term uint64, leaderId string, prevLogIndex uint64,
 }
 
 func checkConsistency(prevLogIndex uint64, prevLogTerm uint64, entries []protobuf.LogEntry) bool {
+    if len(entries) <= 0 {
+        return false
+    }
 	return entries[prevLogIndex].GetTerm() == prevLogTerm
 }
 
@@ -70,6 +73,7 @@ func (this *AppendEntryRpc) Execute(state *raftstate.State) *rpcs.Rpc {
 	var error uint64
 	var success bool
 	var prevLogIndex uint64 = this.pMex.GetPrevLogIndex()
+	var prevLogTerm uint64 = this.pMex.GetPrevLogTerm()
 	var entries []protobuf.LogEntry = (*state).GetEntries()
 
 	if role != raftstate.FOLLOWER {
@@ -81,7 +85,7 @@ func (this *AppendEntryRpc) Execute(state *raftstate.State) *rpcs.Rpc {
 		success = false
 		return respondeAppend(id, success, myTerm, -1)
 
-	} else if checkConsistency(prevLogIndex, this.pMex.GetPrevLogTerm(), entries) {
+	} else if checkConsistency(prevLogIndex, prevLogTerm, entries) {
 
 		success = false
 		error = prevLogIndex
