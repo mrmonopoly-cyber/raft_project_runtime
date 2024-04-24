8c40b35d36e3287c25b8519e1b606ef4caae9805
Alberto Damo <alberto.damo@gmail.com>
2024-04-24 07:39:41 +0200

    refactored node.go into node.go and nodeImpo.go

--- raft/internal/node/node.go
+++ raft/internal/node/node.go
@@ -1,12 +1,6 @@
 package node
 
 import (
-	"bytes"
-	"errors"
-	"io"
-	"log"
-
-	//	"log"
 	"net"
 	"raft/internal/node/address"
 	"raft/internal/node/nodeState"
@@ -22,11 +16,6 @@ type Node interface {
     ResetState(lastLogIndex int)
 }
 
-type node struct {
-	addr address.NodeAddress
-    conn net.Conn
-    nodeState nodeState.VolatileNodeState
-}
 
 func NewNode(remoteAddr string, remotePort string, nodeConn net.Conn) (Node) {
 	return &node{
@@ -36,89 +25,3 @@ func NewNode(remoteAddr string, remotePort string, nodeConn net.Conn) (Node) {
 	}
 }
 
-// Read_rpc implements Node.
-func (this *node) Recv() ([]byte, error) {
-
-    buffer := &bytes.Buffer{}
-
-	// Create a temporary buffer to store incoming data
-	tmp := make([]byte, 1024) // Initial buffer size
-
-
-    if this.conn == nil {
-        return nil, errors.New("connection not instantiated")
-    }
-    //log.Println("want to read")
-    //log.Printf("start reading from %v\n", this.GetIp())
-
-    var bytesRead int = len(tmp)
-    var errConn error
-    var errSavi error
-    for bytesRead == len(tmp){
-		// Read data from the connection
-		bytesRead, errConn = this.conn.Read(tmp)
-
-        // Write the read data into the buffer
-        _, errSavi = buffer.Write(tmp[:bytesRead])
-        
-        // check error saving
-        if errSavi != nil {
-            return nil, errSavi
-        }
-
-		if errConn != nil {
-			if errConn != io.EOF {
-				// Handle other errConnors
-				return nil, errConn
-			}
-
-            if errConn == io.EOF {
-                return nil, errConn
-            }
-			break
-		}
-	}
-
-    //log.Printf("end reading from %v : %v\n", this.GetIp(), buffer)
-    
-    //log.Println("found no error, received message: ", buffer)
-	return buffer.Bytes(), nil 
-
-}
-
-func (this *node)GetNodeState() *nodeState.VolatileNodeState{
-    return &this.nodeState   
-}
-
-func (this *node) CloseConnection(){
-    (*this).conn.Close()
-}
-
-func (this *node) AddConn(conn net.Conn) {
-	this.conn = conn
-}
-
-func (this *node) Send(mex []byte) error{
-    if this.conn == nil {
-        return errors.New("Connection with node " + this.GetIp() + " not enstablish, Dial Done?")
-    }
-//    log.Printf("start sending message to %v", this.GetIp())
-    var _,err = this.conn.Write(mex)
-    if err != nil {
-        log.Panicf("error sending to %v, error %v\n", (*this).GetIp(), err)
-    }
-  //  log.Printf("message sended to %v", this.GetIp())
-    return nil
-	
-}
-
-func (this *node) GetIp() string {
-	return this.addr.GetIp()
-}
-
-func (this *node) GetPort() string {
-	return this.addr.GetPort()
-}
-func (this *node) ResetState(lastLogIndex int){
-    (*this).nodeState.InitVolatileState(lastLogIndex)
-}
