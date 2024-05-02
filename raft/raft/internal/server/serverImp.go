72501ac31a7ba1282ce2ef65c0835c6f7c506d48
Alberto Damo <alberto.damo@gmail.com>
2024-05-02 12:20:55 +0200

    updated run code

--- raft/raft/internal/server/serverImp.go
+++ raft/raft/internal/server/serverImp.go
@@ -42,11 +42,12 @@ type server struct {
 func (s *server) Start() {
     log.Println("Start accepting connections")
     s._state.StartElectionTimeout()
-    // s.wg.Add(1)
-    // go func ()  {
-    //     defer s.wg.Done()
-    //     s.acceptIncomingConn()
-    // }()
+    s.wg.Add(1)
+    go func ()  {
+        defer s.wg.Done()
+        s.acceptIncomingConn()
+    }()
+
     s.wg.Add(1)
     go s.run()
     s.wg.Wait()
