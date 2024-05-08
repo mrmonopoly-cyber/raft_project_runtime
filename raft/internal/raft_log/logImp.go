ddea2d8e18b85ac39632011bf0d2d5e33b72a5b5
Alberto Damo <alberto.damo@gmail.com>
2024-05-08 08:18:58 +0200

    added mutex system to logEntry package

--- raft/internal/raft_log/logImp.go
+++ raft/internal/raft_log/logImp.go
@@ -6,9 +6,11 @@ import (
 	clusterconf "raft/internal/raftstate/clusterConf"
 	p "raft/pkg/raft-rpcProtobuf-messages/rpcEncoding/out/protobuf"
 	"strings"
+	"sync"
 )
 
 type log struct {
+    lock        sync.RWMutex
 	entries     []*p.LogEntry
 	lastApplied int
 	commitIndex int64
@@ -47,14 +49,21 @@ func (this *log) UpdateConfiguration(confOp clusterconf.CONF_OPE, nodeIps []stri
 }
 
 func (this *log) GetEntries() []*p.LogEntry {
+    this.lock.RLock()
+    defer this.lock.Unlock()
 	return this.entries
 }
 
 func (this *log) LastLogIndex() int {
+    this.lock.RLock()
+    defer this.lock.Unlock()
 	return len(this.entries) - 1
 }
 
 func (this *log) More_recent_log(last_log_index int64, last_log_term uint64) bool {
+    this.lock.RLock()
+    defer this.lock.Unlock()
+
 	if last_log_index >= this.commitIndex {
 		var entries []*p.LogEntry = this.GetEntries()
 		if len(entries) <= int(last_log_index) {
@@ -69,12 +78,18 @@ func (this *log) More_recent_log(last_log_index int64, last_log_term uint64) boo
 }
 
 func (this *log) AppendEntries(newEntries []*p.LogEntry) {
+    this.lock.Lock()
+    defer this.lock.Unlock()
+
     l.Println("Append Entries, before: ",this.entries)
     this.entries = append(this.entries,newEntries...)
     l.Println("Append Entries, after: ",this.entries)
 }
 
 func (this *log) UpdateLastApplied() error {
+    this.lock.Lock()
+    defer this.lock.Unlock()
+
 	l.Printf("check if can apply some logEntry: commIndex:%v, lastApplied:%v\n", this.commitIndex, this.lastApplied)
 	for int(this.commitIndex) > this.lastApplied {
 		var entry *p.LogEntry = this.entries[this.commitIndex]
