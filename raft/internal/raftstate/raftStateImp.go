7eaf46991c04db7a694cead93294d7de6dde010a
Alberto Damo <alberto.damo@gmail.com>
2024-05-14 09:45:30 +0200

    debgu

--- raft/internal/raftstate/raftStateImp.go
+++ raft/internal/raftstate/raftStateImp.go
@@ -128,7 +128,7 @@ func (this *raftStateImpl) AppendEntries(newEntries []*p.LogEntry) {
 		this.log.IncreaseCommitIndex()
 		return
 	}
-    log.Println("leader, request to send log Entry to follower")
+    log.Printf("leader, request to send log Entry to follower: ch %v, idx: %v\n", this.leaderEntryToCommit, this.log.GetCommitIndex()+1)
 	this.leaderEntryToCommit <- this.log.GetCommitIndex() + 1
 
     var m int64
