937a935d3ce621374598fc9a91569aa650020a4a
Alberto Damo <alberto.damo@gmail.com>
2024-05-29 12:06:09 +0200

    disabling new feature for debugging

--- raft/internal/raftstate/raftStateImp.go
+++ raft/internal/raftstate/raftStateImp.go
@@ -130,14 +130,14 @@ func (this *raftStateImpl) GetEntries() []*p.LogEntry {
 
 func (this *raftStateImpl) AppendEntries(newEntries []*p.LogEntry) []chan int {
 	var res = this.log.AppendEntries(newEntries)
-    var removingNodeInTwoNodeConf bool = 
-        this.GetNumberNodesInCurrentConf() ==2 && 
-        len(newEntries) == 1 && 
-        newEntries[0].OpType == p.Operation_DELETE
-    //HACK: single case approach to manage the deletion of a node in 2 note conf
+    // var removingNodeInTwoNodeConf bool = 
+    //     this.GetNumberNodesInCurrentConf() ==2 && 
+    //     len(newEntries) == 1 && 
+    //     newEntries[0].OpType == p.Operation_DELETE
+    // //HACK: single case approach to manage the deletion of a node in 2 note conf
 	if !this.Leader() ||
-        this.GetNumberNodesInCurrentConf() == 1 ||
-        removingNodeInTwoNodeConf {
+        this.GetNumberNodesInCurrentConf() == 1{
+        // removingNodeInTwoNodeConf {
             log.Println("auto commit entry")
             this.log.IncreaseCommitIndex()
             if this.Leader() {
