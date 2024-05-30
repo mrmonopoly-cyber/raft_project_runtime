fc637ec7dbb9c8e8ce3b8a8de39e59a9130d0030
Alberto Damo <alberto.damo@gmail.com>
2024-05-30 14:15:16 +0200

    possible fix for bug of not seeing other nodes in the network

--- env_setup/iso_creation/airootfs/root/nmap_discover.sh
+++ env_setup/iso_creation/airootfs/root/nmap_discover.sh
@@ -3,5 +3,12 @@
 work=/usr/share/raft
 others_ip=${work}/others_ip
 others_ip_temp=${work}/others_ip.temp
+
+ip_assigned=$(ip a | grep "10.0.0")
+
+if -z $ip_assigned ; then
+    sleep 5
+fi
+
 nmap 10.0.0.0/24 -sn | grep report | cut -d' ' -f5 | tail -n +2 | head -n -1 > $others_ip_temp
 mv $others_ip_temp $others_ip
