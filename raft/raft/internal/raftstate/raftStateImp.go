7c5df824615cec05b48c052a65aee1de5a6218d6
Alberto Damo <alberto.damo@gmail.com>
2024-06-03 01:29:37 +0200

    updated run code

--- raft/raft/internal/raftstate/raftStateImp.go
+++ raft/raft/internal/raftstate/raftStateImp.go
@@ -231,7 +231,7 @@ func (this *raftStateImpl) AppendEntries(newEntries []*l.LogInstance) {
 	//INFO: thir case of OR happens if there are two nodes in the cluster, the follower drops
 	// and so the leader does not have to ask the dropped follower the ack the deletion of that
 	// node
-	if this.role == LEADER ||
+	if this.role == FOLLOWER ||
 		numNodeInConf <= 1 ||
 		(this.role == LEADER && numNodeInConf == 2) {
 		log.Println("auto commit entry: ", newEntries[0].Entry)
