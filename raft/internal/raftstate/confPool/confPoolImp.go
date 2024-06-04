60b586ccb4d405a147582d3cb82a2cfcad99d3aa
Alberto Damo <alberto.damo@gmail.com>
2024-06-04 13:15:33 +0200

    Revert "logging, debug"
    This reverts commit 71ac157e2bcbc7336adb62cc6127bfef1589107c.

--- raft/internal/raftstate/confPool/confPoolImp.go
+++ raft/internal/raftstate/confPool/confPoolImp.go
@@ -188,7 +188,6 @@ func (c *confPool) AppendEntry(entry *raft_log.LogInstance) {
 			<-entry.Committed
 		}
 		<-entry.Committed
-        log.Println("debug ok")
 		switch {
 		case entry.Entry.OpType == protobuf.Operation_COMMIT_CONFIG_ADD:
 			c.mainConf = c.newConf
