0501d09c2c1696cc5ce5e84d5f90c98405db116a
Alberto Damo <alberto.damo@gmail.com>
2024-06-04 15:30:33 +0200

    comparing only the new conf to add, dangerous

--- raft/internal/raftstate/confPool/confPoolImp.go
+++ raft/internal/raftstate/confPool/confPoolImp.go
@@ -8,6 +8,7 @@ import (
 	"raft/internal/raftstate/confPool/queue"
 	singleconf "raft/internal/raftstate/confPool/singleConf"
 	"raft/pkg/raft-rpcProtobuf-messages/rpcEncoding/out/protobuf"
+	"reflect"
 	"strings"
 	"sync"
 )
@@ -164,7 +165,7 @@ func (c *confPool) AppendEntry(entry *raft_log.LogInstance) {
 		confFiltered = append(confFiltered, c.mainConf.GetConfig()...)
 		var newConf = singleconf.NewSingleConf(c.fsRootDir, confFiltered, &c.nodeList)
 		log.Println("checking conf is the same: ", newConf, c.newConf)
-		if c.newConf != newConf {
+		if !reflect.DeepEqual(c.newConf.GetConfig(),newConf.GetConfig()) {
             c.confQueue.Push(tuple{SingleConf: newConf, LogInstance: entry})
 			return
 		}
