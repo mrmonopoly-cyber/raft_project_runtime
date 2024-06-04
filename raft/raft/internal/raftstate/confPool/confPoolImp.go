a3fab3c60fa1e076257664f44ce8cbeab7732971
Alberto Damo <alberto.damo@gmail.com>
2024-06-04 04:46:03 +0200

    updated run code

--- raft/raft/internal/raftstate/confPool/confPoolImp.go
+++ raft/raft/internal/raftstate/confPool/confPoolImp.go
@@ -157,8 +157,10 @@ func (c *confPool) AppendEntry(entry *raft_log.LogInstance) {
 		confFiltered = append(confFiltered, c.mainConf.GetConfig()...)
 		var newConf = singleconf.NewSingleConf(c.fsRootDir, confFiltered, &c.nodeList)
 		c.confQueue.Push(tuple{SingleConf: newConf, LogInstance: entry})
+        log.Println("waiting conf pushed: ",newConf)
 		for c.newConf != nil {
 		} //HACK: POLLING WAIT
+        log.Println("checking conf is the same: ",newConf, c.newConf)
 		if c.newConf != newConf {
 			return
 		}
