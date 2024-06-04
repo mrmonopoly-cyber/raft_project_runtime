582162dbdd286755e196182d3bcf5a43ea3bf3f7
Alberto Damo <alberto.damo@gmail.com>
2024-06-04 04:53:51 +0200

    updated run code

--- raft/raft/internal/raftstate/confPool/confPoolImp.go
+++ raft/raft/internal/raftstate/confPool/confPoolImp.go
@@ -190,6 +190,7 @@ func (c *confPool) AppendEntry(entry *raft_log.LogInstance) {
 }
 
 func (c *confPool) joinNextConf() {
+    c.emptyNewConf <- 1
 	for {
 		<-c.emptyNewConf
 		<-c.confQueue.WaitEl()
