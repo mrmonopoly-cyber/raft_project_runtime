7c40b314013f7f082f78e25b242b6d9facca0292
Alberto Damo <alberto.damo@gmail.com>
2024-06-04 15:30:54 +0200

    updated run code

--- raft/raft/internal/raftstate/confPool/confPoolImp.go
+++ raft/raft/internal/raftstate/confPool/confPoolImp.go
@@ -165,8 +165,8 @@ func (c *confPool) AppendEntry(entry *raft_log.LogInstance) {
 		confFiltered = append(confFiltered, c.mainConf.GetConfig()...)
 		var newConf = singleconf.NewSingleConf(c.fsRootDir, confFiltered, &c.nodeList)
 		log.Println("checking conf is the same: ", newConf, c.newConf)
-		if !reflect.DeepEqual(c.newConf,newConf){
-		// if c.newConf != newConf {
+        //WARN: DANGEROUS
+		if !reflect.DeepEqual(c.newConf.GetConfig(),newConf.GetConfig()) {
             c.confQueue.Push(tuple{SingleConf: newConf, LogInstance: entry})
 			return
 		}
