d6098a547f5bebcb0677fca5cf7574970caf0d94
Alberto Damo <alberto.damo@gmail.com>
2024-06-07 13:40:42 +0200

    updated run code

--- raft/raft/internal/raft_log/logImp.go
+++ raft/raft/internal/raft_log/logImp.go
@@ -60,6 +60,7 @@ func (this *logEntryImp) GetEntriAt(index int64) *LogInstance {
 		index = 0
 	}
 
+    log.Println("GetEntriAt, full log and size: ",this.entries, this.logSize)
 	if index < int64(this.logSize) {
 		return &this.entries[index]
 	}
