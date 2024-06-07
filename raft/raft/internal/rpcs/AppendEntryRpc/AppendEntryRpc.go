b83630a19eaaf176f06f57f25e3a82cd49c6a9f4
Alberto Damo <alberto.damo@gmail.com>
2024-06-08 00:24:49 +0200

    updated run code

--- raft/raft/internal/rpcs/AppendEntryRpc/AppendEntryRpc.go
+++ raft/raft/internal/rpcs/AppendEntryRpc/AppendEntryRpc.go
@@ -112,7 +112,7 @@ func (this *AppendEntryRpc) Execute(
     var err error
 
     var resp rpcs.Rpc = nil
-    var leaderCommit int64
+    // var leaderCommit int64
 
     if this.pMex.GetTerm() < myTerm { //case 1
         return respondeAppend(id, false, myTerm, -1)
@@ -137,12 +137,11 @@ func (this *AppendEntryRpc) Execute(
                 resp = respondeAppend(id, false, myTerm, nextIdx)
             default:
                 intLog.AppendEntry(newEntriesWrapper, int(prevLogIndex))
-                //FIX: check if the entry are already in the log
-                leaderCommit = this.pMex.GetLeaderCommit()
+                // leaderCommit = this.pMex.GetLeaderCommit()
 
-                if leaderCommit > intLog.GetCommitIndex() {
-                    intLog.MinimumCommitIndex(uint(leaderCommit))
-                }
+                // if leaderCommit > intLog.GetCommitIndex() {
+                //     intLog.MinimumCommitIndex(uint(leaderCommit))
+                // }
                 resp = respondeAppend(id, true , myTerm, intLog.LastLogIndex())
         }
     } 
