22de7205881ac9274dddef835b73c1aad365d652
Alberto Damo <alberto.damo@gmail.com>
2024-06-07 18:15:43 +0200

    updated run code

--- raft/raft/internal/raft_log/logImp.go
+++ raft/raft/internal/raft_log/logImp.go
@@ -167,7 +167,7 @@ func (this *logEntryImp) NewLogInstanceBatch(entry []*protobuf.LogEntry, post []
 //utility
 
 func (this *logEntryImp) isInLog(entry *protobuf.LogEntry, index int) bool{
-    if index >= int(this.logSize){
+    if index >= int(this.logSize) || this.logSize == 0{
         return false
     }
 
