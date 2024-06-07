db9b1631aefa3762eda26a97a00d5d3f2cf878ac
Alberto Damo <alberto.damo@gmail.com>
2024-06-07 20:00:13 +0200

    debug

--- raft/internal/raftstate/confPool/confPoolImp.go
+++ raft/internal/raftstate/confPool/confPoolImp.go
@@ -97,6 +97,8 @@ func (c *confPool) appendEntryToConf(){
         log.Println("appendEntryToConf: waiting signal")
         <- c.entryToCommiC
         var newConf singleconf.SingleConf
+
+        color.Red("explode3")
         var entry = c.GetEntriAt(c.GetCommitIndex()+1)
 
         switch entry.Entry.OpType {
@@ -183,6 +185,7 @@ func (c *confPool) increaseCommitIndex() {
 func (c *confPool) updateLastApplied() {
 	for {
         var toApplyIdx = <- c.ApplyEntryC()
+        color.Red("explode3")
 		var entr = c.GetEntriAt(int64(toApplyIdx))
 
         switch entr.Entry.OpType {
