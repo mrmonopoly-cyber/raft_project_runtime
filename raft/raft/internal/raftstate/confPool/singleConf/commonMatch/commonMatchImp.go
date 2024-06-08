6e4e7e86b5352f4e104a43390c05a2d5f54b0e81
Alberto Damo <alberto.damo@gmail.com>
2024-06-08 03:50:15 +0200

    updated run code

--- raft/raft/internal/raftstate/confPool/singleConf/commonMatch/commonMatchImp.go
+++ raft/raft/internal/raftstate/confPool/singleConf/commonMatch/commonMatchImp.go
@@ -38,15 +38,6 @@ func (c *commonMatchImp) updateCommonMatchIndex()  {
                         c.commitEntryC <- 1
                         c.commonMatchIndex++
                         c.numStable=1
-                        //INFO: it's possible that a node has a mach greater match index 
-                        //and so every time i increment the current common i have to check if
-                        //the other nodes has already a greater common match index
-                        // for _, v := range c.subs {
-                        //     if v.Trd >= c.commonMatchIndex{
-                        //         c.numStable++
-                        //     }
-                        //
-                        // }
                     }
                 }
                 v.Trd = newMatch
