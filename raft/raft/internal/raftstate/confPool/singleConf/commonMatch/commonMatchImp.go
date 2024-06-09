6eef599f8f3604a8d00f2a608c4cb23d472970df
Alberto Damo <alberto.damo@gmail.com>
2024-06-09 15:33:30 +0200

    updated run code

--- raft/raft/internal/raftstate/confPool/singleConf/commonMatch/commonMatchImp.go
+++ raft/raft/internal/raftstate/confPool/singleConf/commonMatch/commonMatchImp.go
@@ -1,16 +1,18 @@
 package commonmatch
 
 import (
-	"log"
 	nodestate "raft/internal/raftstate/confPool/NodeIndexPool/nodeState"
 	"raft/internal/utiliy"
 
 	"github.com/fatih/color"
 )
 
+type substriber = utiliy.Triple[nodestate.NodeState, <-chan int, int]
+
+
 type commonMatchImp struct {
 	commitEntryC     chan int
-	subs             []utiliy.Triple[nodestate.NodeState, <-chan int, int]
+	subs             []substriber
 	numNodes         int
 	numStable        uint
 	commonMatchIndex int
@@ -27,34 +29,6 @@ func (c *commonMatchImp) StopNotify() {
 // CommitNewEntryC implements CommonMatch.
 func (c *commonMatchImp) CommitNewEntryC() <-chan int {
 	return c.commitEntryC
-}
-
-// utility
-func (c *commonMatchImp) updateCommonMatchIndex() {
-	var halfNodeNum = c.numNodes / 2
-
-	for _, v := range c.subs {
-		go func() {
-			for c.run{
-				var newMatch = <-v.Snd
-				color.Red("check if can increase commonMatchIdx: %v,%v,%v,%v",
-					c.numNodes, newMatch, c.commonMatchIndex, v.Trd)
-				if newMatch > c.commonMatchIndex && v.Trd < newMatch {
-					color.Red("check passed: %v:%v:%v:%v\n",
-						c.numNodes, newMatch, c.commonMatchIndex, v.Trd)
-					c.numStable++
-					if c.numStable > uint(halfNodeNum) {
-                        if c.commitEntryC != nil{
-                            c.commitEntryC <- c.commonMatchIndex
-                        }
-						c.commonMatchIndex++
-						c.numStable = 1
-					}
-				}
-				v.Trd = newMatch
-			}
-		}()
-	}
 }
 
 func NewCommonMatchImp(initialCommonCommitIdx int, nodeSubs map[string]nodestate.NodeState) *commonMatchImp {
@@ -82,11 +56,6 @@ func NewCommonMatchImp(initialCommonCommitIdx int, nodeSubs map[string]nodestate
         res.numNodes++
         j++
     }
-
-	log.Println("list subs commmon Match: ", res.subs)
-
-	go res.updateCommonMatchIndex()
-
 	return res
 }
 
@@ -113,4 +82,28 @@ func (c *commonMatchImp) addUpdatedNode(indexTrpCell uint, state nodestate.NodeS
     trp.Fst = state
     _,trp.Snd = state.Subscribe(nodestate.MATCH)
     trp.Trd = state.FetchData(nodestate.MATCH)
+
+	go c.checkUpdateNewMatch(trp)
+}
+func (c *commonMatchImp) checkUpdateNewMatch(sub *substriber) {
+    var halfNodeNum = c.numNodes/2
+
+    for c.run{
+        var newMatch = <-sub.Snd
+        color.Red("check if can increase commonMatchIdx: %v,%v,%v,%v",
+        c.numNodes, newMatch, c.commonMatchIndex, sub.Trd)
+        if newMatch > c.commonMatchIndex && sub.Trd < newMatch {
+            color.Red("check passed: %v:%v:%v:%v\n",
+            c.numNodes, newMatch, c.commonMatchIndex, sub.Trd)
+            c.numStable++
+            if c.numStable > uint(halfNodeNum) {
+                if c.commitEntryC != nil{
+                    c.commitEntryC <- c.commonMatchIndex
+                }
+                c.commonMatchIndex++
+                c.numStable = 1
+            }
+        }
+        sub.Trd = newMatch
+    }
 }
